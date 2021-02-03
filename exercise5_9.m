39e11288c9b2336cc260918d1c5326711ffc0f0d
NickKaparinos <36237128+NickKaparinos@users.noreply.github.com>
2021-02-03 14:14:00 +0200

    Add files via upload

--- exercise5_9.m
+++ exercise5_9.m
@@ -19,19 +19,17 @@ R2 = zeros(2,1);
 adjustedR2 = zeros(2,1);
 
 Rsq = @(ypred,y) 1-sum((ypred-y).^2)/sum((y-mean(y)).^2);
-adjRsq = @(ypred,y,n,k) ( 1 - (n-1)/(n-1-k)*sum((ypred-y).^2)/sum((y-mean(y)).^2) );
+adjRsq = @(ypred,y,n,dof) ( 1 - (n-1)/(n-dof)*sum((ypred-y).^2)/sum((y-mean(y)).^2) );
 
 % Normal linear regression
 regressionModel = fitlm(X,Y);
-b = table2array(regressionModel.Coefficients);
-b = b(:,1);
+b = regressionModel.Coefficients.Estimate;
 
 Ypred = [ones(length(X),1) X]*b;
 errors = Y - Ypred;
-rmse(1) = sqrt( 1/(length(Y)-length(b))*sum(errors).^2);
+rmse(1) = sqrt( 1/(length(Y)-length(b))*sum(errors.^2) );
 R2(1) = Rsq(Ypred,Y);
 adjustedR2(1) = adjRsq(Ypred,Y,length(Y),length(b));
-
 
 % Step wise regression
 [b,~,~,model,stats] = stepwisefit(X,Y);
